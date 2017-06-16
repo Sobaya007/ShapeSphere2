429a398efd64f5b1bf1757a39b88f1c0dfb3bdb0
Ark <target.ark@gmail.com>
2017-06-16 22:48:40 +0900

    alureInitDeviceのassertをコメントアウト

--- source/sbylib/wrapper/al/AL.d
+++ source/sbylib/wrapper/al/AL.d
@@ -5,11 +5,11 @@ import derelict.alure.alure;
 import sbylib.setting;
 
 class AL {
-    
+
     static void init() {
         DerelictAL.load(AL_DLL_PATH);
         DerelictALURE.load(ALURE_DLL_PATH);
-        assert(alureInitDevice(null, null), "Failed to init alure!");
+        //assert(alureInitDevice(null, null), "Failed to init alure!");
     }
 
     static void terminate() {
