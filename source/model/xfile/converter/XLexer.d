1515e0749e9470f0a8452f1260b716c61d5c18dd
Ark <target.ark@gmail.com>
2017-07-16 19:14:43 +0900

    add test

--- source/model/xfile/converter/XLexer.d
+++ source/model/xfile/converter/XLexer.d
@@ -86,20 +86,22 @@ unittest {
         "header\n" ~
         "po { // commentout\n" ~
         "0.0,1;\n" ~
+        "\"sobaya.homo\"\n" ~
         "}\n";
 
     lexer.run(src);
     assert(!lexer.empty);
 
     XToken[] tokens = lexer.get();
-    assert(tokens.length == 7);
+    assert(tokens.length == 8);
     assert(cast(XTokenLabel)tokens[0]      && tokens[0].lexeme == "po");
     assert(cast(XTokenLeftParen)tokens[1]  && tokens[1].lexeme == "{");
     assert(cast(XTokenLabel)tokens[2]      && tokens[2].lexeme == "0.0");
     assert(cast(XTokenComma)tokens[3]      && tokens[3].lexeme == ",");
     assert(cast(XTokenLabel)tokens[4]      && tokens[4].lexeme == "1");
     assert(cast(XTokenSemicolon)tokens[5]  && tokens[5].lexeme == ";");
-    assert(cast(XTokenRightParen)tokens[6] && tokens[6].lexeme == "}");
+    assert(cast(XTokenLabel)tokens[6] && tokens[6].lexeme == "\"sobaya.homo\"");
+    assert(cast(XTokenRightParen)tokens[7] && tokens[7].lexeme == "}");
 
     lexer.clear();
     assert(lexer.empty);
