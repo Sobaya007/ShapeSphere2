ed7b9e904666aec887728ad3a41e91edd526c035
Sobaya <sobaya007@gmail.com>
2017-10-13 21:10:49 +0900

    再代入防止した

--- source/sbylib/material/compass/CompassMaterial.d
+++ source/sbylib/material/compass/CompassMaterial.d
@@ -11,25 +11,51 @@ class CompassMaterialUniformKeeper : UniformKeeper {
 
     mixin MaterialUtils.declare;
 
-    Watcher!(uvec2) xvec, yvec, zvec;
+    private Watcher!(uvec2) _xvec;
+    private Watcher!(uvec2) _yvec;
+    private Watcher!(uvec2) _zvec;
+
+    void xvec(vec2 v) {
+        this._xvec.get() = v;
+    }
+
+    vec2 xvec() {
+        return _xvec;
+    }
+
+    void yvec(vec2 v) {
+        this._yvec.get() = v;
+    }
+
+    vec2 yvec() {
+        return _yvec;
+    }
+
+    void zvec(vec2 v) {
+        this._zvec.get() = v;
+    }
+
+    vec2 zvec() {
+        return _zvec;
+    }
 }
 
 class CompassMaterial : MaterialTemp!CompassMaterialUniformKeeper {
 
     this(Camera camera) {
         super((CompassMaterialUniformKeeper keeper) {
-            keeper.xvec = new Watcher!(uvec2)((ref uvec2 xvec) {
+            keeper._xvec = new Watcher!(uvec2)((ref uvec2 xvec) {
                 xvec = normalize((camera.worldMatrix.toMatrix3() * vec3(1,0,0)).xy);
             }, new uvec2("xvec"));
-            keeper.yvec = new Watcher!(uvec2)((ref uvec2 yvec) {
+            keeper._yvec = new Watcher!(uvec2)((ref uvec2 yvec) {
                 yvec = normalize((camera.worldMatrix.toMatrix3() * vec3(0,1,0)).xy);
             }, new uvec2("yvec"));
-            keeper.zvec = new Watcher!(uvec2)((ref uvec2 zvec) {
+            keeper._zvec = new Watcher!(uvec2)((ref uvec2 zvec) {
                 zvec = normalize((camera.worldMatrix.toMatrix3() * vec3(0,0,1)).xy);
             }, new uvec2("zvec"));
-            keeper.xvec.addWatch(camera.worldMatrix);
-            keeper.yvec.addWatch(camera.worldMatrix);
-            keeper.zvec.addWatch(camera.worldMatrix);
+            keeper._xvec.addWatch(camera.worldMatrix);
+            keeper._yvec.addWatch(camera.worldMatrix);
+            keeper._zvec.addWatch(camera.worldMatrix);
         });
     }
 }
