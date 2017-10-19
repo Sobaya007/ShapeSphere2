4529a38a21e1e9ac0420e91a6b1194f6356f43a2
Ark <target.ark@gmail.com>
2017-10-20 03:08:16 +0900

    デバッグ出力を消した

--- source/examples/EditorExample.d
+++ source/examples/EditorExample.d
@@ -50,7 +50,6 @@ void editorExample() {
         a.zIndex = 2;
         t += 50;
         control.add(a);
-        button.writeln;
     });
 
     auto leftComponent = new ComponentListComponent(
@@ -71,34 +70,6 @@ void editorExample() {
     rightComponent.x = w/2;
     rightComponent.y = h;
     control.add(rightComponent);
-
-    // auto spacer = new SpacerComponent(0, h, 0, 400, 300);
-    // control.add(spacer);
-    // auto label = new LabelComponent(0.0, h-300, 0, "D is 神"d, 50, vec4(0.6, 0.7, 0.8, 1.0));
-    // control.add(label);
-    // auto checkBox = new CheckBoxComponent(w/2, h, 0, 40);
-    // control.add(checkBox);
-    // auto button = new ButtonComponent(w/2, h/4*3, 0, 300, 40, "犯人は"d, 25);
-    //
-    //
-    // dstring[] ary = ["ONONONON!!!!", "アカーーーーン！！！！", "簡単すぎィィィ！！！！"];
-    // control.add(button);
-    // auto dropDown = new DropDownComponent(w/3, h/5*2, 0, 400, 40, ary, 25, vec4(1, 1, 1, 1));
-    // control.add(dropDown);
-    // auto groupBox = new GroupBoxComponent(10, h/3, 1, 200, "バイ成ィ"d, new CheckBoxComponent(0, 0, 0, 40));
-    // control.add(groupBox);
-    //
-    // int t = 0;
-    // button.setTrigger({
-    //     import std.stdio;
-    //     import std.algorithm;
-    //     int i = dropDown.getIndex;
-    //     dstring po = i<0 ? "@_n_ari！！！！"d : ary[i];
-    //     auto a = new LabelComponent(10.0, h-20-t, 2, po, 50, vec4(0, 0, 0, 1.0));
-    //     t += 50;
-    //     control.add(a);
-    //     button.writeln;
-    // });
 
 
     core.addProcess(render, "render");
